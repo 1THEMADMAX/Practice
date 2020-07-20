2655d9e9f7bf78b943ec015c2409cc56429356a4
Madmax <1Madmax@list.ru>
2020-07-20 18:38:56 +0300

    patch 0.2.0

--- Git1.py
+++ Git1.py
@@ -3,9 +3,9 @@ def _sqr(x):
     return x*x
 def _tri(x):
     return x*x*x
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-z = input('Введите операцию')
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+z = input('Введите операцию: ')
 
 if z == '+':
     print(a + b)
@@ -15,6 +15,7 @@ elif z == '*':
     print(a * b)
 elif z=='/':
     print( '%2.3f' %(a/b))
-
-
-
+elif z=='**':
+    print(_sqr(a))
+elif z=='***':
+    print(_tri(a))
