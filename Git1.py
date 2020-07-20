ceb3422350bed8aeff160b265fd4dbdcd9aca7cc
Madmax <1Madmax@list.ru>
2020-07-20 18:19:01 +0300

    patch 0.1.0

--- Git1.py
+++ Git1.py
@@ -2,9 +2,13 @@ import math
 
 a = int(input('Введите первое число'))
 b = int(input('Введите второе число'))
-z = int(input('Введите операцию'))
+z = input('Введите операцию')
 
 if z == '+':
     print(a + b)
-
-    
+elif z=='-':
+    print(a - b)
+elif z == '*':
+    print(a * b)
+elif z=='/':
+    print( '%2.3f' %(a/b))
